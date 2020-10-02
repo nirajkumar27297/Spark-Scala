2e650a45fb4d7425cd9af8544b1d23221402f49a
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-10-02 23:22:49 +0530

    [Niraj]Added:Scala spark codes to predict stock price using pipeline

--- SparkScalaQuestions/src/main/scala/StockPrediction/StockPredictionPipeline.scala
+++ SparkScalaQuestions/src/main/scala/StockPrediction/StockPredictionPipeline.scala
@@ -1,13 +1,14 @@
 package StockPrediction
 
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
-import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
+import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.{avg, col}
 
-object StockPrediction extends App {
+object StockPredictionPipeline extends App {
   def createSession(): SparkSession = {
     val spark = SparkSession
       .builder()
@@ -20,7 +21,7 @@ object StockPrediction extends App {
 
   def metricsEvaluation(test: DataFrame) {
     val prediction = lrModel.transform(test)
-    println("The r-squared value is %1.2f".format(lrModel.summary.r2))
+    prediction.show()
     val evaluator = new RegressionEvaluator()
       .setLabelCol("Close")
       .setPredictionCol("prediction")
@@ -34,39 +35,28 @@ object StockPrediction extends App {
     println(s"The error percentage is %1.2f%%".format(errorValue))
   }
 
-  def preProcessingAndAssembling(inputDF: DataFrame): DataFrame = {
+  def preProcessingAndAssembling(): VectorAssembler = {
     val assembler = new VectorAssembler()
-      .setInputCols(inputDF.drop("Close", "Adj Close", "Date").columns)
+      .setInputCols(Array("Open", "High", "Low", "Volume"))
       .setOutputCol("features")
-
-    val assembledOutput =
-      assembler
-        .transform(inputDF.drop())
-        .select("Close", "features")
-    assembledOutput.show()
-    assembledOutput
+    assembler
   }
 
-  def scalingData(inputDF: DataFrame): DataFrame = {
+  def scalingData(): StandardScaler = {
     val scaler =
       new StandardScaler()
         .setInputCol("features")
         .setOutputCol("scaledFeatures")
-    val scalerModel = scaler.fit(inputDF)
-    val scaledDF =
-      scalerModel.transform(inputDF).select("Close", "scaledFeatures")
-    scaledDF.show()
-    scaledDF
+    scaler
   }
 
-  def modelCreation(inputDF: DataFrame): LinearRegressionModel = {
-    val lr =
+  def modelCreation(): LinearRegression = {
+    val linearRegression =
       new LinearRegression()
         .setFeaturesCol("scaledFeatures")
         .setLabelCol("Close")
 
-    val lrModel = lr.fit(inputDF)
-    lrModel
+    linearRegression
   }
 
   def takeInput(filepath: String): DataFrame = {
@@ -83,14 +73,16 @@ object StockPrediction extends App {
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.ERROR)
   val inputDF = takeInput("./src/test/resources/GOOG.csv")
-  val assembledDF = preProcessingAndAssembling(inputDF)
-  val scaledDF = scalingData(assembledDF)
-  val split = scaledDF.randomSplit(Array(0.80, 0.20), 111)
+  val assembler = preProcessingAndAssembling()
+  val scaler = scalingData()
+  val linearRegression = modelCreation()
+  val pipeline =
+    new Pipeline().setStages(Array(assembler, scaler, linearRegression))
+  val split = inputDF.randomSplit(Array(0.80, 0.20), 111)
   val train = split.apply(0)
   val test = split.apply(1)
-  val lrModel = modelCreation(train)
+  val lrModel = pipeline.fit(train)
   lrModel.write.overwrite().save("./src/test/resources/model")
   metricsEvaluation(test)
   spark.stop()
-
 }
