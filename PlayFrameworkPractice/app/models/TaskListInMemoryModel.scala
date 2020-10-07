04e29e979b4e18fc833910818cfca58d725b7bb7
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-10-07 22:16:03 +0530

    [Niraj]Added:Scala codes to perform GET and POST using play framework

---
+++ PlayFrameworkPractice/app/models/TaskListInMemoryModel.scala
@@ -0,0 +1,34 @@
+package models
+
+import scala.collection.mutable
+
+
+object TaskListInMemoryModel {
+
+  private val users = mutable.Map[String,String]("Mark" -> "pass")
+  private val tasks = mutable.Map[String,List[String]]("Mark" -> List("Make Videos","eat","Sleep"))
+
+  def validateUser(username:String,password:String):Boolean = {
+    users.get(username).map(_ == password).getOrElse(false)
+  }
+
+  def createUser(username:String,password:String):Boolean = {
+    if(users.contains(username))
+      false
+    else {
+      users(username) = password
+      true
+    }
+  }
+
+  def getTasks(username:String):Seq[String] = {
+    tasks.get(username).getOrElse(Nil)
+  }
+
+  def addTask(username:String,tasks:String) = ???
+
+  def removeTask(username:String,index:Int):Boolean = ???
+
+
+
+}
