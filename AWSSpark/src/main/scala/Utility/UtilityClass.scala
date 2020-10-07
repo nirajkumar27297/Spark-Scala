ccf1b75dbee5fef0146a33a6f9fa8d696a10c673
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-10-07 13:34:53 +0530

    Niraj[Refracted]:Utlity class added for creating required objects

---
+++ AWSSpark/src/main/scala/Utility/UtilityClass.scala
@@ -0,0 +1,36 @@
+/*
+The objective is to create a utility class which can be passed over
+different classes to create the required session and context object if Required
+@author:Niraj Kumar
+ */
+
+
+
+package Utility
+
+import org.apache.spark.SparkConf
+
+import org.apache.spark.sql.SparkSession
+
+object UtilityClass {
+
+  /* Function to Create Spark Session Object
+  @return SparkSession
+   */
+  def createSessionObject(): SparkSession = {
+
+    val sparkconfigurations = new SparkConf()
+      .setAppName("Stock Prediction Application")
+      .setMaster("local")
+
+    val sparkSessionObj = SparkSession
+      .builder()
+      .appName("StockPredictionPythonConnectivity")
+      .config(sparkconfigurations)
+      .getOrCreate()
+    sparkSessionObj
+  }
+  //TODO Implement create Context Function if required
+
+
+}
