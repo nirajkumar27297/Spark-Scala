f1f08ae4e89df89a914935580d33d49a6105d6c4
nirajkumar27297 <nirajkumar17315@gmail.com>
2020-10-16 23:18:05 +0530

    [Niraj]Added:Spark scala codes for real time stock prediction using kafka

---
+++ Kafka/RealTimeStockPrediction/pythonFiles/StockPricePrediction.py
@@ -0,0 +1,36 @@
+"""
+The objective is to take input from a standard Input,create a dataframe from that
+Load the LinearRegressionModel which we have dumped earlier and predict the Close Price
+and return it using Standard Output
+Library Used -
+1> pandas [For Creating DataFrames]
+  Version - 1.1.2
+2> pickle [For Dumping Model]
+  Version - 3.8
+3> sys [For taking Standard Input and Passing Output Through Standard Output]
+@author : Niraj Kumar
+"""
+
+import sys
+import pandas as pd
+import pickle
+
+
+# Taking Input Through Standard Input
+inputStockPriceFile = sys.stdin
+
+# Creating dataframe from standard input and transposing it
+inputStockPriceDF = pd.read_csv(inputStockPriceFile, header=None)
+# Providing name to the columns
+inputStockPriceDF.columns = {"Open", "High", "Low", "Volume"}
+print(inputStockPriceDF.head(10))
+# inputStockPriceDF["Volume"] = inputStockPriceDF["Volume"].apply(lambda x : x.replace("]",""))
+# inputStockPriceDF["Open"] = inputStockPriceDF["Open"].apply(lambda x : x.replace("[",""))
+# # Loading the dumped LinearRegressionModel pickle file
+# with open("./MachineLearningModel/PythonModel/StockPriceModel.pkl", "rb") as modelFile:
+#     linearRegressionModel = pickle.load(modelFile)
+#
+# # Predicting the stock close price
+# predictedClosePrice = linearRegressionModel.predict(inputStockPriceDF)
+# # Returning the output through standard output
+# sys.stdout.write(str(-2))
